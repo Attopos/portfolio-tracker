099a07f71f5e32445f9b282826a7ed918800ca56
AC <shizhengcao74519@gmail.com>
2026-03-18 17:41:49 +0800

    chore: add more icons

--- client/src/features/assets/assetIcons.js
+++ client/src/features/assets/assetIcons.js
@@ -1,9 +1,27 @@
-import btcIcon from "../../assets/asset-icons/btc.png";
-import ethIcon from "../../assets/asset-icons/eth.png";
+import btcIcon from "./icons/btc.svg";
+import dax40Icon from "./icons/dax40.svg";
+import ethIcon from "./icons/eth.svg";
+import goldIcon from "./icons/gold.svg";
+import nasdaq100Icon from "./icons/nasdaq100.svg";
+import nikkei225Icon from "./icons/nikkei225.svg";
+import sp500Icon from "./icons/S&P500.svg";
 
 const ASSET_ICON_MAP = {
   BTC: btcIcon,
   ETH: ethIcon,
+  QQQ: nasdaq100Icon,
+  NASDAQ: nasdaq100Icon,
+  NASDAQ100: nasdaq100Icon,
+  DAX: dax40Icon,
+  DAX40: dax40Icon,
+  NIKKEI: nikkei225Icon,
+  NIKKEI225: nikkei225Icon,
+  N225: nikkei225Icon,
+  GOLD: goldIcon,
+  XAU: goldIcon,
+  SPX: sp500Icon,
+  "S&P500": sp500Icon,
+  SP500: sp500Icon,
 };
 
 function normalizeAssetIconKey(value) {
